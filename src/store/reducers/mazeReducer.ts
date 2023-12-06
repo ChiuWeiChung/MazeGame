78c1b0e572555c35e73d53797349d066c9503ab6
rickchiu <rick@jgallop.com>
2023-12-06 18:41:16 +0800

    update npm version

--- src/store/reducers/mazeReducer.ts
+++ src/store/reducers/mazeReducer.ts
@@ -14,7 +14,7 @@ const initialState = {
 }
 
 
-const mazeReducer = (state: Maze = initialState, action: Action) => {
+const mazeReducer:any = (state: Maze = initialState, action: Action) => {
     switch (action.type) {
         case (ActionTypes.renderRunner):
             return { ...state, position: action.payload.position, pacManForward: action.payload.pacManForward,step:action.payload.count+1 };
